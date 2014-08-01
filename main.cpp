b15acafd81519a4238fd6f3eb1170f3d78644ec8
LuaxY <yann.guineau@voidmx.net>
2014-08-01 22:09:17 +0200

    refactor(main): code optimisation

--- main.cpp
+++ main.cpp
@@ -10,15 +10,6 @@ extern "C"
 #include "functions.hpp"
 #include "LuaPlayer.hpp"
 
-void report_errors(lua_State* L, int status)
-{
-    if (status != 0)
-    {
-        std::cerr << "[ERROR] " << lua_tostring(L, -1) << std::endl;
-        lua_pop(L, 1);
-    }
-}
-
 int main()
 {
     lua_State* L = luaL_newstate();
@@ -30,16 +21,7 @@ int main()
     // Objets
     luaopen_Player(L);
 
-    int status = luaL_loadfile(L, "ia.lua");
-    if (status == 0)
-    {
-        status = lua_pcall(L, 0, LUA_MULTRET, 0);
-    }
-    report_errors(L, status);
-
-    /*if (luaL_dofile(L, "ia.lua"))
-        std::cout << lua_tostring(L, -1) << std::endl;*/
-
+    luaL_dofile(L, "ia.lua");
     lua_close(L);
 }
 
